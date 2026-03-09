ef0c47f2f35d1e912ab4a575a649cc29cf2949c3
ammar <ammar.mohammed.6783@gmail.com>
2026-03-10 01:37:05 +0200

    modified items page

--- src/pages/Items.tsx
+++ src/pages/Items.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Nav from "../components/Nav"
 import Navigation from "../components/Navigation"
 
@@ -23,6 +24,30 @@ const Items = () => {
         </Feed>
     */
 
+    const [total, setTotal] = useState(0);
+    const [active, setActive] = useState(10);
+    const [views, setViews] = useState(0);
+    const [tradeOffers, setTradeOffers] = useState(0);
+
+    let cards = [
+        {
+            value: total,
+            text: "Total Items"
+        },
+        {
+            value: active,
+            text: "Active Listings"
+        },
+        {
+            value: views,
+            text: "Total Views"
+        },
+        {
+            value: tradeOffers,
+            text: "Trade Offers"
+        },
+    ];
+
     return (
         <div>
             <Nav />
@@ -31,12 +56,19 @@ const Items = () => {
                 <div className="mx-auto w-7/10">
                     <div className="flex justify-between items-center my-5">
                         <div>
-                            <h2 className="text-2xl font-medium my-2">My Items</h2>
+                            <h2 className="text-3xl font-bold my-2">My Items</h2>
                             <p className="text-gray-500">Manage your Listed items</p>
                         </div>
                         <button className="bg-black text-white px-4 py-2 rounded-lg h-10 flex items-center gap-2">
-                            <IoMdAdd/> Add New Item
+                            <IoMdAdd /> Add New Item
                         </button>
+                    </div>
+                    <div className="flex">
+                        {
+                            cards.map((ele, index) => (
+                                <Card key={index} info={ele} />
+                            ))
+                        }
                     </div>
                 </div>
             </div>
@@ -44,4 +76,19 @@ const Items = () => {
     )
 }
 
-export default Items
+export default Items;
+
+
+type CardInfo = {
+    value: number;
+    text: string;
+};
+
+function Card({ info } : {info: CardInfo}) {
+    return (
+        <div className="rounded-lg p-5 m-1 border border-gray-300 w-full">
+            <p className="font-bold text-2xl">{info.value}</p>
+            <p>{info.text}</p>
+        </div>
+    )
+}
