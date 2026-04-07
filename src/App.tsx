4caf64171c73e73c0d27ef320af44b4109f54f83
ammar <ammar.mohammed.6783@gmail.com>
2026-04-07 20:05:10 +0200

    added favourites routing

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Layout from './components/Layout'
 import AddItem from './pages/AddItem'
 import HowItWork from './pages/HowItWork'
 import Settings from './pages/Settings'
+import Favourites from './pages/Favourites'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
         <Route path='explore' element={<Explore />} />
+        <Route path='favourites' element={<Favourites />} />
         <Route path='items' element={<Items />} />
         <Route path='messages' element={<Messages />} />
         <Route path='profile' element={<Profile />} />
